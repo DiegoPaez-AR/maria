c9fa527bf7a079b756a5a7596f22308088e0c1b0
Maria VPS <maria-vps@paez.is>
2026-05-19 15:03:04 -0300

    ops: consumed inbox 2026-05-19T15:03:01-03:00

--- ops/instances/maria-paez/inbox/redeploy-confold-data.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set +e
-cd /root/secretaria/ops/sites/intensa.io
-bash deploy.sh 2>&1 | grep -E "cache-bust" | head -3
-echo
-echo "Verificar datos de Confold en producción:"
-curl -sk "https://intensa.io/maria/terminos/" | grep -oE '(RUT 218377600011|Pradines Clemente 1795|Montevideo 11500)' | head -3
-echo
-echo "DONE"
