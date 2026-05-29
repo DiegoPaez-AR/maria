f1f105b7fd2d7be7c26ca6b36114d73f6897f08e
Maria VPS <maria-vps@paez.is>
2026-05-29 12:33:04 -0300

    ops: consumed inbox 2026-05-29T12:33:01-03:00

--- ops/instances/maria-paez/inbox/verif-log-hernan.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -uo pipefail
-cd /root/secretaria
-cf=config/instances/maria-paez.conf; set -a; . "$cf"; set +a
-echo "=== ultimas salientes a Hernan (usuario_id=2) ==="
-sqlite3 -header -column "$MARIA_DB" "SELECT datetime(timestamp,'-3 hours') art, direccion, de, COALESCE(json_extract(metadata_json,'\$.tipo'),'') tipo, substr(cuerpo,1,55) FROM eventos WHERE usuario_id=2 AND canal='whatsapp' ORDER BY id DESC LIMIT 4;"
