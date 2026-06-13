2734acc1ca38d7e38d19d12c07b2f51b9204f18d
Maria VPS <maria-vps@paez.is>
2026-06-12 23:45:05 -0300

    ops: consumed inbox 2026-06-12T23:45:01-03:00

--- ops/instances/maria-paez/inbox/DNS-CHECK2.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-echo "== SPF (intensa.io TXT)"
-dig +short intensa.io TXT @8.8.8.8 2>&1
-echo "== DKIM (google._domainkey.intensa.io TXT)"
-dig +short google._domainkey.intensa.io TXT @8.8.8.8 2>&1
-echo "== DMARC"
-dig +short _dmarc.intensa.io TXT @8.8.8.8 2>&1
