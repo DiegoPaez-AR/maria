f51d579b5f434c8ebc048045fa84e09ac98e41b0
Maria VPS <maria-vps@paez.is>
2026-07-01 14:08:05 -0300

    ops: consumed inbox 2026-07-01T14:08:01-03:00

--- ops/instances/maria-paez/inbox/borrar-evento-test2.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-OUT_DIR="$(dirname "$0")/../outbox"; mkdir -p "$OUT_DIR"; OUT="$OUT_DIR/borrar-evento-test2.out"
-PORT="${ASISTENTE_INTERNAL_PORT:?}"; SECRET="${ASISTENTE_INTERNAL_SECRET:?}"
-{
-echo "=== borrar_evento up8hrfda1r63glv9itn05garcc (cal diego@paez.is) ==="
-curl -s -m 25 -X POST "http://127.0.0.1:$PORT/accion" -H "X-Intensa-Secret: $SECRET" -H "Content-Type: application/json" \
- -d '{"usuarioId":1,"accion":{"tipo":"borrar_evento","id":"up8hrfda1r63glv9itn05garcc","calendarId":"diego@paez.is"}}'; echo
-} > "$OUT" 2>&1
-echo done >> "$OUT"
