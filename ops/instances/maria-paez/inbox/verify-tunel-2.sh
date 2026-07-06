bb806e222406149ffc20a936eeb5586af259bb08
Maria VPS <maria-vps@paez.is>
2026-07-05 23:32:08 -0300

    ops: consumed inbox 2026-07-05T23:32:01-03:00

--- ops/instances/maria-paez/inbox/verify-tunel-2.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-IP=$(curl -s -m 15 --socks5-hostname 127.0.0.1:1080 https://ifconfig.me 2>/dev/null)
-echo "túnel (launchd): ${IP:-FALLO}"
-[ -n "$IP" ] && curl -s -m 10 --socks5-hostname 127.0.0.1:1080 "http://ip-api.com/line/?fields=country,isp" 2>/dev/null
-echo LISTO
