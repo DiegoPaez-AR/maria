9d436fa1b5d5356101e30a3af8d48259bd01bb91
Maria VPS <maria-vps@paez.is>
2026-07-05 23:54:07 -0300

    ops: consumed inbox 2026-07-05T23:54:01-03:00

--- ops/instances/maria-paez/inbox/verify-tunel-tapa.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-for i in 1 2 3; do
-  IP=$(curl -s -m 12 --socks5-hostname 127.0.0.1:1080 https://ifconfig.me 2>/dev/null)
-  echo "intento $i: ${IP:-sin respuesta}"
-  [ -n "$IP" ] && break
-  sleep 8
-done
-echo LISTO
