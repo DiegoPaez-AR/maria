424b0e8a79583b181076d28601289dc986d6dd30
Maria VPS <maria-vps@paez.is>
2026-07-07 19:09:04 -0300

    ops: consumed inbox 2026-07-07T19:09:01-03:00

--- ops/instances/maria-paez/inbox/CANCELAR-860-861.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -e
-DB="${MARIA_DB:?falta MARIA_DB}"
-sqlite3 "$DB" "UPDATE programados SET enviado=1, metadata_json=json_set(COALESCE(metadata_json,'{}'),'\$.cancelado','vencido durante bloqueo WA 2026-07-07') WHERE enviado=0 AND id IN (860,861);"
-echo "quedan pendientes:"
-sqlite3 "$DB" "SELECT id, cuando, substr(texto,1,40) FROM programados WHERE enviado=0;"
