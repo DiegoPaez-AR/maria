fd735548a1827977e88397894e7ace47d6ff5ac1
Maria VPS <maria-vps@paez.is>
2026-07-07 22:58:10 -0300

    ops: consumed inbox 2026-07-07T22:58:01-03:00

--- ops/instances/maria-paez/inbox/RELOAD-FIRMA-POS.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -e
-cd /root/secretaria
-pm2 reload ecosystem.config.js --only maria-paez --update-env >/dev/null 2>&1
-sleep 5
-pm2 jlist | python3 -c "import json,sys; [print(p['name'], p['pm2_env']['status']) for p in json.load(sys.stdin) if p['name']=='maria-paez']"
-grep -c "reEmailLinea" /root/secretaria/google.js
