0b5f3b2eeb573d125a7aea5895118c6d2622e848
Maria VPS <maria-vps@paez.is>
2026-08-14 18:11:05 -0300

    ops: consumed inbox 2026-08-14T18:11:01-03:00

--- ops/instances/maria-paez/inbox/diag-ulises.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-node -e "
-const db = require('/root/secretaria/node_modules/better-sqlite3')(process.env.MARIA_DB, {readonly:true});
-const rows = db.prepare(\"SELECT id, usuario_id, nombre, whatsapp, email, visibilidad FROM contactos WHERE nombre LIKE '%Ulises%' OR nombre LIKE '%Esteban%'\").all();
-console.log(rows.length ? JSON.stringify(rows, null, 1) : 'NO existen Ulises ni Esteban en ninguna libreta');
-db.close();
-"
-echo LISTO
