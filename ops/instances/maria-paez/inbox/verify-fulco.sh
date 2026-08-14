28b87af3326750da7cbd4b353ee76fcf7db4792a
Maria VPS <maria-vps@paez.is>
2026-08-14 18:29:05 -0300

    ops: consumed inbox 2026-08-14T18:29:01-03:00

--- ops/instances/maria-paez/inbox/verify-fulco.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-node -e "
-const db = require('/root/secretaria/node_modules/better-sqlite3')(process.env.MARIA_DB, {readonly:true});
-const rows = db.prepare(\"SELECT id, timestamp, canal, direccion, substr(cuerpo,1,130) c FROM eventos WHERE usuario_id=2 AND timestamp >= datetime('now','-12 minutes') ORDER BY id\").all();
-rows.forEach(r => console.log(r.id, r.timestamp, r.canal, r.direccion, '|', String(r.c).replace(/\n/g,' ')));
-db.close();
-"
-echo LISTO
