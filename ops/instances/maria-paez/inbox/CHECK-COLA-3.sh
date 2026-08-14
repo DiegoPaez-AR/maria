50c9e71acc6cbf00be69a67945412db15ff4ba8c
Maria VPS <maria-vps@paez.is>
2026-08-14 09:57:05 -0300

    ops: consumed inbox 2026-08-14T09:57:02-03:00

--- ops/instances/maria-paez/inbox/CHECK-COLA-3.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-DB="${MARIA_DB:?falta MARIA_DB}"
-sqlite3 "$DB" "SELECT id, estado, intentos, COALESCE(tomado_en,'-'), COALESCE(entregado,'-'), substr(texto,1,28) FROM wa_outbox ORDER BY id DESC LIMIT 4;"
