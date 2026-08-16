9fc13af917bed86f7b9e4c013befbf0b6291a828
Maria VPS <maria-vps@paez.is>
2026-08-16 20:10:07 -0300

    ops: consumed inbox 2026-08-16T20:10:01-03:00

--- ops/instances/maria-paez/inbox/reload-fase2.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-cd /root/secretaria
-[ -f state/.canary-bad-commit ] && { echo "CANARY BLOQUEÓ: $(cat state/.canary-bad-commit)"; exit 0; }
-pm2 reload ecosystem.config.js --update-env >/dev/null 2>&1 && echo "reload OK"
-node -e "require('/root/secretaria/gmail-handler.js'); console.log('require gmail-handler OK')" 2>&1 | tail -1
-echo LISTO
