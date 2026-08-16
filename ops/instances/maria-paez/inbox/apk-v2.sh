fc392849f0e51e9c83e3f10e02e2c3a85dfe8426
Maria VPS <maria-vps@paez.is>
2026-08-16 09:09:05 -0300

    ops: consumed inbox 2026-08-16T09:09:01-03:00

--- ops/instances/maria-paez/inbox/apk-v2.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-pgrep -f mb-build-worker >/dev/null && echo compilando || echo terminó
-grep -E "APK_OK|APK_FAIL|BUILD (SUCC|FAIL)|^e: file" /root/mariabridge-build.log | tail -6
-echo LISTO
