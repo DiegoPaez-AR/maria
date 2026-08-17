3adf09a661f813ec6ceeb8c0f6fbc11a48cb4053
Maria VPS <maria-vps@paez.is>
2026-08-17 01:45:06 -0300

    ops: consumed inbox 2026-08-17T01:45:01-03:00

--- ops/instances/maria-paez/inbox/ver-migracion-tg.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-node -e "
-const db=require('/root/secretaria/node_modules/better-sqlite3')(process.env.MARIA_DB,{readonly:true});
-const cols=db.prepare('PRAGMA table_info(contactos)').all().map(c=>c.name);
-console.log('contactos.telegram:', cols.includes('telegram') ? 'MIGRADO ✓' : 'FALTA ✗');
-db.close();"
-grep "migración" ~/.pm2/logs/maria-paez-out.log | tail -3
-echo LISTO
