3115f987b2b9d71d886471e44ad6329984d28c8a
Maria VPS <maria-vps@paez.is>
2026-08-22 16:52:35 -0300

    ops: consumed inbox 2026-08-22T16:52:01-03:00

--- ops/instances/maria-paez/inbox/build-44.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-cd /root/secretaria
-nohup bash /root/mb-build-worker.sh > /tmp/mbbuild.log 2>&1 &
-echo "build lanzado"
-sleep 5
-tail -3 /root/mariabridge-build.log 2>/dev/null
-echo LISTO
