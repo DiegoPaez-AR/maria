f4ef9a4a4610fd296c02d8a3306713b55d519a0f
Maria VPS <maria-vps@paez.is>
2026-08-22 13:16:07 -0300

    ops: consumed inbox 2026-08-22T13:16:01-03:00

--- ops/instances/maria-paez/inbox/ver42b.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-pgrep -f mb-build-worker >/dev/null && echo compilando || echo terminó
-grep -E "BUILD (SUCC|FAIL)|APK_OK|APK_FAIL|^e: file" /root/mariabridge-build.log | tail -3
-curl -s https://intensa.io/_dl/mariabridge-latest.json; echo ""
-echo LISTO
