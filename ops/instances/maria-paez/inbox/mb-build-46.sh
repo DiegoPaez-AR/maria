bb551924165174933048fc2ab6b4f442a000634c
Maria VPS <maria-vps@paez.is>
2026-09-04 09:56:07 -0300

    ops: consumed inbox 2026-09-04T09:56:01-03:00

--- ops/instances/maria-paez/inbox/mb-build-46.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-ls -la /root/mb-build-worker.sh && head -30 /root/mb-build-worker.sh
-nohup bash /root/mb-build-worker.sh > /tmp/mb-build-46.log 2>&1 &
-echo "build lanzado en background (pid $!)"
-echo LISTO
