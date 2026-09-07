2cd44736c61f3e0cdd67492a25895e8247b0d452
Maria VPS <maria-vps@paez.is>
2026-09-07 16:57:30 -0300

    ops: consumed inbox 2026-09-07T16:57:01-03:00

--- ops/instances/maria-paez/inbox/cortar-263.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-cd /root/secretaria
-node -e "
-const mem=require('./memory');
-const r=mem.db.prepare(\"UPDATE wa_outbox SET estado='entregado', entregado=CURRENT_TIMESTAMP WHERE id=263 AND estado='pendiente'\").run();
-console.log('263 cerrado:', r.changes);
-console.log(JSON.stringify(mem.db.prepare('SELECT id,estado,intentos,metadata_json FROM wa_outbox WHERE id>=262').all()));
-"
