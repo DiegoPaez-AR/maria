a6b6954cb096d1becfeaee52e3f055700c9eeb4f
Maria VPS <maria-vps@paez.is>
2026-09-09 09:39:07 -0300

    ops: consumed inbox 2026-09-09T09:39:01-03:00

--- ops/instances/maria-paez/inbox/canary-0909.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-cd /root/secretaria
-ls -la /tmp/canary-tick.log; grep -nE "^not ok|# fail|canary" /tmp/canary-tick.log | head; grep -n "not ok" -A12 /tmp/canary-tick.log | head -40
-git log --oneline -3 | cat; git status --short | head -5
-ls -la ops/instances/maria-paez/snapshots/ | grep -i canary
-echo LISTO
