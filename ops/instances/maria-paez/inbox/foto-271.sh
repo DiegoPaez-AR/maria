24f44a87265fb34357cdd17c1c2e5269c8eebdc2
Maria VPS <maria-vps@paez.is>
2026-09-15 13:14:08 -0300

    ops: consumed inbox 2026-09-15T13:14:01-03:00

--- ops/instances/maria-paez/inbox/foto-271.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-cd /root/secretaria
-mkdir -p ops/instances/maria-paez/shots
-cp /var/www/intensa.io/_dl/verif-271-147bd361.png ops/instances/maria-paez/shots/verif-271.png && ls -la ops/instances/maria-paez/shots/
-ls -t /var/www/intensa.io/_dl/verif-*.png | head -5
-echo LISTO
